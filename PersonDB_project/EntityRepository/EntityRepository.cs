e572a8bb85237f6a0393350ba70590438df86461
OstrovskiyMaxim <maxim.ostrovskiy@gmail.com>
2016-01-20 15:51:00 +0200

    DB Architecture and Entity Settings

---
+++ PersonDB_project/EntityRepository/EntityRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonDB_project;
+using PersonDB_project.Models;
+
+namespace EntityRepository
+{
+    class EntityRepository : IRepository<User>
+    {
+        EntityUserContext db = new EntityUserContext();
+
+        public void Create(User item)
+        {
+            db.Users.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            db.Users.Remove(GetById(id));
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return db.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(User item)
+        {
+            db.Entry<User>(item).State = System.Data.Entity.EntityState.Modified;
+        }
+    }
+}
---
+++ PersonDB_project/PersonDB_project/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonDB_project.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
